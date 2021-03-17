074a762aa51653f2029a5573dfef66518c14b73c
petakolda <peta.kolarik@seznam.cz>
2021-03-17 22:08:42 +0100

    adding Sqrt tests

--- src/test_prg.cpp
+++ src/test_prg.cpp
@@ -25,6 +25,8 @@ TEST_F(BasicOperation, Sum) {
     EXPECT_TRUE(m.Sum(0, values[7]) == values[7]);
     EXPECT_EQ(m.Sum(values[2], values[3]), 481969641);
     EXPECT_EQ(m.Sum(values[3], -values[3]), 0);
+    EXPECT_DOUBLE_EQ(m.Sum(0.3, values[8]), 0.95);
+    EXPECT_DOUBLE_EQ(m.Sum(values[9], values[8]), 0.15);
     double result = values[0];
     for (int i = 0; i < 3; i++) {
         result = m.Sum(result, values[i+1]);
@@ -38,6 +40,8 @@ TEST_F(BasicOperation, Sub) {
     EXPECT_TRUE(m.Subtrac(values[7], values[9]) == 1.3);
     EXPECT_EQ(m.Subtrac(values[5], values[3]), -25846245);
     EXPECT_EQ(m.Subtrac(values[3], values[3]), 0);
+    EXPECT_DOUBLE_EQ(m.Subtrac(0.3, values[8]), -0.35);
+    EXPECT_DOUBLE_EQ(m.Subtrac(values[9], 0.2), -1);
     double result = values[0];
     for (int i = 0; i < arr_size-1; i++) {
         result = m.Subtrac(result, values[i+1]);
@@ -51,7 +55,10 @@ TEST_F(BasicOperation, Divide) {
     EXPECT_FALSE(m.Divide(values[1], values[0]) != 8.5);
     EXPECT_TRUE(m.Divide(values[2], values[9]) == -570154320);
     EXPECT_EQ(m.Divide(values[5], values[7]), -120);
+    EXPECT_EQ(m.Divide(values[8], values[7]), 1.3);
     EXPECT_EQ(m.Divide(0, values[8]), 0);
+    EXPECT_DOUBLE_EQ(m.Divide(27.43, -values[8]), -42.2);
+    EXPECT_DOUBLE_EQ(m.Divide(values[8], 9.5), 0.06842105263157894736842105263158);
     double result = values[0];
     for (int i = 0; i < 3; i++) {
         result = m.Divide(result, values[i+1]);
@@ -65,6 +72,7 @@ TEST_F(BasicOperation, Multiply) {
     EXPECT_TRUE(m.Multiply(values[2], values[3]) == 11789051226615360);
     EXPECT_EQ(m.Multiply(values[7], 0), 0);
     EXPECT_EQ(m.Multiply(1, values[6]), values[6]);
+    EXPECT_DOUBLE_EQ(m.Multiply(0.23, values[9]), -0.184);
     double result = values[0];
     for (int i = 0; i < 6; i++) {
         result = m.Multiply(result, values[i+1]);
@@ -88,6 +96,7 @@ TEST_F(AdvanceOperation, Pow) {
     EXPECT_ANY_THROW(m.Pow(values[1], values[6]));
     EXPECT_FALSE(m.Pow(values[5], values[9]) != 9765625);
     EXPECT_TRUE(m.Pow(values[10], values[1]) == 100000);
+    EXPECT_DOUBLE_EQ(m.Pow(0.6, values[1]), 0.07776);
     EXPECT_EQ(m.Pow(values[1], 1), values[1]);
     EXPECT_EQ(m.Pow(values[1], 0), 1);
     double result = values[0];
@@ -95,4 +104,20 @@ TEST_F(AdvanceOperation, Pow) {
         result = m.Pow(result, values[i+1]);
     }
     EXPECT_EQ(result, 1);
+}
+
+// testovani funkce Sqrt (odmocneni)
+TEST_F(AdvanceOperation, Sqrt) {
+    EXPECT_ANY_THROW(m.Sqrt(2, values[6]));
+    EXPECT_ANY_THROW(m.Sqrt(0, values[1]));
+    EXPECT_FALSE(m.Sqrt(3, -27) != 9);
+    EXPECT_TRUE(m.Sqrt(-2, values[2]) == 0.00004682295);
+    EXPECT_DOUBLE_EQ(m.Sqrt(-0.6, values[7]), 2.050270072);
+    EXPECT_EQ(m.Sqrt(1, values[1]), values[1]);
+    EXPECT_EQ(m.Sqrt(6, 15625), 5);
+    double result = values[0];
+    for (int i = 0; i < 2; i++) {
+        result = m.Sqrt(result, values[i+1]);
+    }
+    EXPECT_EQ(result, 53.9282691);
 }
